05d70e2897a710600f5515a683f778ab1261c54c
Paula George <george.p@hanaboso.com>
2021-08-07 16:21:03 +0200

    tested refresh token exception

--- lib/Authorization/Provider/__tests__/OAuth2Provider.ts
+++ lib/Authorization/Provider/__tests__/OAuth2Provider.ts
@@ -2,6 +2,9 @@ import OAuth2Dto from '../Dto/OAuth2Dto';
 import { ApplicationInstall } from '../../../Application/Database/ApplicationInstall';
 import { AUTHORIZATION_SETTINGS } from '../../../Application/Base/AApplication';
 import { CLIENT_ID, CLIENT_SECRET } from '../../Type/OAuth2/IOAuth2Application';
+import { OAuth2Provider, REFRESH_TOKEN } from '../OAuth2/OAuth2Provider';
+import { TOKEN } from '../../Type/Basic/ABasicApplication';
+import * as faker from 'faker';
 import { OAuth2Provider } from '../OAuth2/OAuth2Provider';
 import AOAuthProvider from '../AOAuthProvider';
 
@@ -14,14 +17,15 @@ jest.mock('../../../Logger/Logger', () => ({
 }));
 
 describe('OAuth2Provider tests', () => {
-  it('authorize ', () => {
-    const authUrl = 'https://identity.idoklad.cz/server/connect/authorize';
-    const user = 'user';
-    const app = 'testApp';
-    const scope = ['idoklad_api', 'offline_access'];
+  const authUrl = 'https://identity.idoklad.cz/server/connect/authorize';
+  const user = 'user';
+  const app = 'testApp';
+  const scope = ['idoklad_api', 'offline_access'];
 
-    const oauth2Provider = new OAuth2Provider('testBackend');
-    const appInstall = new ApplicationInstall();
+  const oauth2Provider = new OAuth2Provider('testBackend');
+  const appInstall = new ApplicationInstall();
+
+  it('authorize ', () => {
     // eslint-disable-next-line max-len
     appInstall.addSettings({ [AUTHORIZATION_SETTINGS]: { [CLIENT_SECRET]: '**469040-****-4e03-861e-e19da38*****', [CLIENT_ID]: '**469040-****-4e03-861e-e19da38*****' } });
     const dto = new OAuth2Dto(appInstall, authUrl, 'https://identity.idoklad.cz/server/connect/token');
@@ -29,6 +33,14 @@ describe('OAuth2Provider tests', () => {
     expect(oauth2Provider.authorize(dto, scope)).toEqual(
       'https://identity.idoklad.cz/server/connect/authorize?response_type=code&client_id=**469040-****-4e03-861e-e19da38*****&redirect_uri=testBackend%2Fapi%2Fapplications%2Fauthorize%2Ftoken&scope=idoklad_api%2Coffline_access&state=dXNlcjp0ZXN0QXBw&access_type=offline',
     );
+  });
+
+  it('it should throw an exception when refresh the user token and the token is not found inside the request ', () => {
+    const dto = new OAuth2Dto(appInstall, authUrl, 'https://identity.idoklad.cz/server/connect/token');
+    const errorMsg = 'Message [Refresh token not found! Refresh is not possible.] code [205]';
+    expect(oauth2Provider.refreshAccessToken(dto, {
+      refreshToken: faker.internet.password(),
+    })).rejects.toThrowError(errorMsg);
   });
 
   it('throwException ', () => {
