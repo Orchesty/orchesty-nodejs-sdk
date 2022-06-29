8368ab175f24dce5b474b3ea6825aa1d1740f0aa
Radek Jirsa <jirsa.r@hanaboso.com>
2022-06-29 15:11:48 +0200

    Deps updated

--- lib/Batch/ABatchNode.ts
+++ lib/Batch/ABatchNode.ts
@@ -2,6 +2,6 @@ import AConnector from '../Connector/AConnector';
 import BatchProcessDto from '../Utils/BatchProcessDto';
 import { IBatchNode } from './IBatchNode';
 
-export default abstract class ABatchConnector extends AConnector implements IBatchNode {
+export default abstract class ABatchNode extends AConnector implements IBatchNode {
   public abstract processAction(dto: BatchProcessDto): Promise<BatchProcessDto> | BatchProcessDto;
 }
