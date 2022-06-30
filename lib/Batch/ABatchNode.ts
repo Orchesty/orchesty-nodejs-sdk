b6bec63957bd95439d30782ae8b07139c872bb90
Jakub Sobotka <sobotka.j@hanaboso.com>
2022-06-30 14:21:31 +0000

    Change setters in ABatchConnector

--- lib/Batch/ABatchNode.ts
+++ lib/Batch/ABatchNode.ts
@@ -1,7 +1,28 @@
 import AConnector from '../Connector/AConnector';
 import BatchProcessDto from '../Utils/BatchProcessDto';
 import { IBatchNode } from './IBatchNode';
+import CurlSender from '../Transport/Curl/CurlSender';
+import { IApplication } from '../Application/Base/IApplication';
+import MongoDbClient from '../Storage/Mongodb/Client';
 
 export default abstract class ABatchNode extends AConnector implements IBatchNode {
   public abstract processAction(dto: BatchProcessDto): Promise<BatchProcessDto> | BatchProcessDto;
+
+  public setSender(sender: CurlSender): ABatchNode {
+    this.sender = sender;
+
+    return this;
+  }
+
+  public setApplication(application: IApplication): ABatchNode {
+    this.application = application;
+
+    return this;
+  }
+
+  public setDb(db: MongoDbClient): ABatchNode {
+    this.db = db;
+
+    return this;
+  }
 }
