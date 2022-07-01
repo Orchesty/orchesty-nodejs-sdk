9ee37fc74e992ffd812013760c1620c746138ac0
David Horacek <horacek.d@hanaboso.com>
2022-07-01 09:24:20 +0200

    remove setters

--- lib/Connector/AConnector.ts
+++ lib/Connector/AConnector.ts
@@ -3,14 +3,18 @@ import ACommonNode from '../Commons/ACommonNode';
 import ResultCode from '../Utils/ResultCode';
 import ResponseDto from '../Transport/Curl/ResponseDto';
 import AProcessDto from '../Utils/AProcessDto';
-import { IApplication } from '../Application/Base/IApplication';
-import MongoDbClient from '../Storage/Mongodb/Client';
 
 export default abstract class AConnector extends ACommonNode {
   // eslint-disable-next-line @typescript-eslint/naming-convention
   protected sender?: CurlSender;
 
   protected _okStatuses = [200, 201];
+
+  public setSender(sender: CurlSender): this {
+    this.sender = sender;
+
+    return this;
+  }
 
   public evaluateStatusCode(res: ResponseDto, dto: AProcessDto, message?: string): boolean {
     if (this._okStatuses.includes(res.responseCode)) {
@@ -23,24 +27,6 @@ export default abstract class AConnector extends ACommonNode {
     );
 
     return false;
-  }
-
-  public setApplication(application: IApplication): AConnector {
-    this.application = application;
-
-    return this;
-  }
-
-  public setDb(db: MongoDbClient): AConnector {
-    this.db = db;
-
-    return this;
-  }
-
-  public setSender(sender: CurlSender): AConnector {
-    this.sender = sender;
-
-    return this;
   }
 
   protected get _sender(): CurlSender {
