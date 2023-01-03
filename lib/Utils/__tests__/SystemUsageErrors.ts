e1c9b2469af01f7760178aa55af48ae8bcbb09e7
Radek Jirsa <jirsa.r@ghanaboso.com>
2023-01-03 16:31:07 +0100

    Bump version

--- lib/Utils/__tests__/SystemUsageErrors.ts
+++ lib/Utils/__tests__/SystemUsageErrors.ts
@@ -6,18 +6,18 @@ describe('Test system usages', () => {
 
         expect(cpuTimes).toHaveProperty('cpuUserCodeTime');
         expect(typeof cpuTimes.cpuUserCodeTime).toBe('number');
-        expect(cpuTimes.cpuUserCodeTime > 0).toBeTruthy();
+        expect(cpuTimes.cpuUserCodeTime >= 0).toBeTruthy();
         expect(cpuTimes).toHaveProperty('cpuKernelCodeTime');
         expect(typeof cpuTimes.cpuKernelCodeTime).toBe('number');
-        expect(cpuTimes.cpuKernelCodeTime > 0).toBeTruthy();
+        expect(cpuTimes.cpuKernelCodeTime >= 0).toBeTruthy();
         expect(cpuTimes).toHaveProperty('cpuStartTime');
         expect(typeof cpuTimes.cpuStartTime).toBe('number');
-        expect(cpuTimes.cpuStartTime > 0).toBeTruthy();
+        expect(cpuTimes.cpuStartTime >= 0).toBeTruthy();
     });
 
     it('getCpuUsage', () => {
         const cpuUsage = getCpuUsage();
         expect(typeof cpuUsage).toBe('number');
-        expect(cpuUsage > 0).toBeTruthy();
+        expect(cpuUsage >= 0).toBeTruthy();
     });
 });
