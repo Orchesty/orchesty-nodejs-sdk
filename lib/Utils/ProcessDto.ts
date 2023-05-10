b88ce393f7b08a1be3fe82c0ab4663f3eb6eb47a
Radek Jirsa <jirsa.r@ghanaboso.com>
2023-05-10 09:06:20 +0200

    ProcessDto: added factory method for forms

--- lib/Utils/ProcessDto.ts
+++ lib/Utils/ProcessDto.ts
@@ -1,6 +1,19 @@
 import AProcessDto from './AProcessDto';
+import { CORRELATION_ID, NODE_ID } from './Headers';
 
 export default class ProcessDto<Data = unknown> extends AProcessDto<Data> {
+
+    public static createForFormRequest(appName: string, user: string, correlationId: string, nodeId = 'form'): ProcessDto {
+        const p = new ProcessDto();
+        p.setHeaders({
+            [CORRELATION_ID]: correlationId,
+            [NODE_ID]: nodeId,
+        });
+        p.setUser(user);
+        p.setCurrentApp(appName);
+
+        return p;
+    }
 
     public setData(data: string): this {
         this.data = data;
